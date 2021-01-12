4107d1e9e3638eb43aa97ecb719627f7bfa0a10b
黄冠桦 <huangguanhua@yodinfo.com>
2021-01-12 11:01:09 +0800

    feat: add link

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -2,6 +2,7 @@
   <div class="footer">
     <div class="footer-content">
       <div><a href="https://github.com/">GitHub</a></div>
+      <div><a href="https://es6.ruanyifeng.com/">es6</a></div>
     </div>
   </div>
 </template>
