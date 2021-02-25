52a894d66751f0cd404bda68c0cbbd955d7b2fce
黄冠桦 <huangguanhua@yodinfo.com>
2021-02-25 16:02:38 +0800

    feat: 公安备案

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -3,16 +3,21 @@
     <div class="footer-content">
       <div><a href="https://github.com/">GitHub</a></div>
       <div><a href="https://es6.ruanyifeng.com/">es6</a></div>
-      <div><a href="https://beian.miit.gov.cn">桂ICP备2021000407号-1</a></div>
+      <div style="margin:0 auto;display:flex">
+        <a href="https://beian.miit.gov.cn" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;margin-right:10px">桂ICP备2021000407号-1</a>
+		 		<a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44030602005463" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;"><img :src="recordIcon" style="float:left;"/><p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">粤公网安备 44030602005463号</p></a>
+		 	</div>
     </div>
   </div>
 </template>
 
 <script>
+import recordIcon from '../../assets/recordIcon.png'
 export default {
   name: 'com-head',
   data(){
     return {
+      recordIcon
     }
   },
   methods:{
